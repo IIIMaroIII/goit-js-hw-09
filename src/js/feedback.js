3dbafa62e05f8d8581e5d2454643e2ff522bd230
Rodion Matvieiev <matveev.r.v@icloud.com>
2024-01-22 20:29:20 -0500

    update

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -2,44 +2,9 @@ import '../css/feedback-form.css';
 
 const STORAGE_KEY = 'feedback-form-state';
 
-// const refs = {
-//   form: document.querySelector('.feedback-form'),
-//   email: document.querySelector('.feedback-email'),
-//   message: document.querySelector('.feedback-message'),
-// };
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
 
-// refs.form.addEventListener('submit', onFormSubmit);
-// refs.form.addEventListener('input', onFormInput);
-
-// populateFormInput();
-
-// function onFormInput(e) {
-//   formData[e.target.name] = e.target.value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//   console.log(formData);
-//   e.target.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-
-// function populateFormInput() {
-//   const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-//   if (formData) {
-//     refs.form.email.value = formData['email'];
-//     refs.form.message.value = formData['message'];
-//   }
-// }
-
-// ------------//
-// function populateFormInput() {
-//   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-//   if (savedData) {
-//     refs.form.email.value = savedData['email'];
-//     refs.form.message.value = savedData['message'];
-//   }
-
-//   console.log({ email, message });
-// }
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onFormInput);
