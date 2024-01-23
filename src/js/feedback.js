e490d1587c9b3e4552315d1898641b408fdd30f5
Rodion Matvieiev <matveev.r.v@icloud.com>
2024-01-22 20:25:31 -0500

    update

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,35 +1,46 @@
 import '../css/feedback-form.css';
 
-const formData = {};
-const STORAGE_KEY = 'feedback-form-state';
+// const formData = {};
+// const STORAGE_KEY = 'feedback-form-state';
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-email'),
-  message: document.querySelector('.feedback-message'),
-};
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   email: document.querySelector('.feedback-email'),
+//   message: document.querySelector('.feedback-message'),
+// };
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', onFormInput);
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.form.addEventListener('input', onFormInput);
 
-populateFormInput();
+// populateFormInput();
 
-function onFormInput(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
+// function onFormInput(e) {
+//   formData[e.target.name] = e.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
 
-function onFormSubmit(e) {
-  e.preventDefault();
-  e.target.reset();
-  console.log(formData);
-  localStorage.removeItem(STORAGE_KEY);
-}
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   console.log(formData);
+//   e.target.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// }
 
-function populateFormInput() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (savedData) {
-    refs.email.children[0].value = savedData['email'];
-    refs.message.children[0].value = savedData['message'];
-  }
-}
+// function populateFormInput() {
+//   const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   if (formData) {
+//     refs.form.email.value = formData['email'];
+//     refs.form.message.value = formData['message'];
+//   }
+// }
+
+// ------------//
+// function populateFormInput() {
+//   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   if (savedData) {
+//     refs.form.email.value = savedData['email'];
+//     refs.form.message.value = savedData['message'];
+//   }
+
+//   console.log({ email, message });
+// }
