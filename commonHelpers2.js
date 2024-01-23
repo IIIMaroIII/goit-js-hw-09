42deb213fa7debba5b5059b93c41df36bf018ab3
IIIMaroIII <107584063+IIIMaroIII@users.noreply.github.com>
2024-01-23 01:29:35 +0000

    Deploying to gh-pages from @ IIIMaroIII/goit-js-hw-09@3dbafa62e05f8d8581e5d2454643e2ff522bd230 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";const e={form:document.querySelector(".feedback-form")};e.form.addEventListener("submit",onFormSubmit);e.form.addEventListener("input",onFormInput);
+//# sourceMappingURL=commonHelpers2.js.map
